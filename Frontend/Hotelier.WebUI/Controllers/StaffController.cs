a2cb47dc821996161aa971385f1db3e820d22273
mirayipekli <mirayipekli@gmail.com>
2024-09-03 20:55:25 +0300

    Updated staff

--- Frontend/Hotelier.WebUI/Controllers/StaffController.cs
+++ Frontend/Hotelier.WebUI/Controllers/StaffController.cs
@@ -51,6 +51,7 @@ namespace Hotelier.WebUI.Controllers
             return View();
         }
 
+
         public async Task<IActionResult> DeleteStaff(int id)
         {
             var client = _httpClientFactory.CreateClient();
@@ -58,6 +59,34 @@ namespace Hotelier.WebUI.Controllers
             if (responseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateStaff(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:36176/api/Staff/{id}");
+            if (responseMessage.IsSuccessStatusCode) 
+            { 
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateStaffViewModel>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateStaff(UpdateStaffViewModel model)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(model);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync($"http://localhost:36176/api/Staff/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                 return RedirectToAction("Index");
             }
             return View();
         }
