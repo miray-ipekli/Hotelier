9d7e58a6fb746dbab9352e294a42718b2f53db8e
mirayipekli <mirayipekli@gmail.com>
2024-09-05 00:09:35 +0300

    Add Create User DTO class

---
+++ Frontend/Hotelier.WebUI/Controllers/RegisterController.cs
@@ -0,0 +1,28 @@
+﻿using Hotelier.EntityLayer.Concrete;
+using Hotelier.WebUI.Dtos.AppUserDto;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hotelier.WebUI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CreateNewUserDto createNewUserDto)
+        {
+            return View();
+        }
+    }
+}
