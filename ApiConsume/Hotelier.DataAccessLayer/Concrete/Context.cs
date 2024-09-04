c643540c3ced8b4a39bf582a288bcae269d848d6
mirayipekli <mirayipekli@gmail.com>
2024-09-04 23:56:10 +0300

    Add Identity

--- ApiConsume/Hotelier.DataAccessLayer/Concrete/Context.cs
+++ ApiConsume/Hotelier.DataAccessLayer/Concrete/Context.cs
@@ -1,4 +1,5 @@
 ﻿using Hotelier.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -8,10 +9,14 @@ using System.Threading.Tasks;
 
 namespace Hotelier.DataAccessLayer.Concrete
 {
-    public class Context : DbContext
+    public class Context : IdentityDbContext<AppUser, AppRole, int>
     {
-        public Context(DbContextOptions<Context> options) : base(options) { }
+        //public Context(DbContextOptions<Context> options) : base(options) { }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=MIRAY\\SQLEXPRESS;initial catalog=DbHotelier;integrated security=true");
+        }
         public DbSet<Room> Rooms { get; set; }
         public DbSet<Service> Services { get; set; }
         public DbSet<Staff> Staffs { get; set; }
--- ApiConsume/Hotelier.DataAccessLayer/Migrations/20240904203259_identity.Designer.cs
+++ ApiConsume/Hotelier.DataAccessLayer/Migrations/20240904203259_identity.Designer.cs
@@ -4,14 +4,16 @@ using Hotelier.DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Hotelier.DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20240904203259_identity")]
+    partial class identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
