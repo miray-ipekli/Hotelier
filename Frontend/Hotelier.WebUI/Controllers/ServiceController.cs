0f7581aac599d69571b4717e237569e8b7d80115
mirayipekli <mirayipekli@gmail.com>
2024-09-04 22:34:39 +0300

    Update Service

--- Frontend/Hotelier.WebUI/Controllers/ServiceController.cs
+++ Frontend/Hotelier.WebUI/Controllers/ServiceController.cs
@@ -66,5 +66,37 @@ namespace Hotelier.WebUI.Controllers
             }
             return View();
         }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateService(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:36176/api/Service/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateServiceDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateService(UpdateServiceDto updateServiceDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateServiceDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync($"http://localhost:36176/api/Service/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
--- Frontend/Hotelier.WebUI/Dtos/ServiceDto/UpdateServiceDto.cs
+++ Frontend/Hotelier.WebUI/Dtos/ServiceDto/UpdateServiceDto.cs
@@ -1,6 +1,17 @@
-﻿namespace Hotelier.WebUI.Dtos.ServiceDto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hotelier.WebUI.Dtos.ServiceDto
 {
     public class UpdateServiceDto
     {
+        public int ServiceID { get; set; }
+        [Required(ErrorMessage = "Hizmet ikon linki giriniz.")]
+        public string ServiceIcon { get; set; }
+        [Required(ErrorMessage = "Hizmet başlığı giriniz.")]
+        [StringLength(100, ErrorMessage = "Hizmet başlığı en fazla 100 karakter olabilir.")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Hizmet açıklaması giriniz.")]
+        [StringLength(500, ErrorMessage = "Hizmet açıklaması en fazla 500 karakter olabilir.")]
+        public string Description { get; set; }
     }
 }
